a81e62cc8e6250fb8cf13e8fbf205ca0d982679b
Le Gusto <janusonis.gustas@gmail.com>
2022-07-05 16:55:01 +0300

    Add password matching and improve accesability

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-let pass1 = document.getElementById("pas");
+let pass1 = document.getElementById("pass");
 let pass2 = document.getElementById("con_pas");
 let form = document.querySelector("form");
 
-form.addEventListener('submit', function (event) {
-    if(pass1.textContent != pass2.textContent) {
-      // If it isn't, we display an appropriate error message
-      showError();
-      // Then we prevent the form from being sent by canceling the event
-      event.preventDefault();
-    }
-  });
+
+pass2.addEventListener('input', (e)=>{
+  if (pass2.value !== pass1.value) {
+    pass2.setCustomValidity("Passwords do not match!");
+
+  }
+  else {
+    pass2.setCustomValidity("");
+  }
+})
